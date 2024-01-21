fd7bf983bc3fa7f1252ba0bf46a43038b7d378d3
Tetsuya Satoh <satohT@example.com>
2024-01-21 18:51:22 +0900

    コメントがないと分かり辛い部分があったのでコメントを追加。課金プランが取得できなかったときにNoneを返すように変更

--- app/ituens/ITuens.py
+++ app/ituens/ITuens.py
@@ -6,17 +6,20 @@ class ITuens:
     
     store_url_template = 'https://apps.apple.com/{}/app/id{}'
 
-    def getBillings(self, country: str, app_id: int) -> str:
+    def getBillingsOrNone(self, country: str, app_id: int) -> str:
         url = self.store_url_template.format(country, app_id)  
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html5lib')
         elements = soup.find_all(attrs={"class": "list-with-numbers__item"})
 
         billing_list = []
-        for element in elements:
-            billing = element.get_text().replace('\n', '').replace(' ', '').replace('¥', ' ¥')
-            billing_list.append(billing)
-        billings = '\n'.join(billing_list)
-        
+        billings = None
+        if (len(elements) >= 1):
+            for element in elements:
+                # 半角スペースがプラン名と価格の間にたくさんある為、一旦除去してから再度追加する。
+                billing = element.get_text().replace('\n', '').replace(' ', '').replace('¥', ' ¥')
+                billing_list.append(billing)
+            billings = '\n'.join(billing_list)
+    
         return billings
     
