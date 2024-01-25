ace98466eb2d7cc8dc9ccbe74632eac77a7130a5
Tetsuya Satoh <satohT@example.com>
2024-01-25 20:44:32 +0900

    アプリのユーザーの国籍ランキングを返す関数を追加

--- app/ituens/ITuens.py
+++ app/ituens/ITuens.py
@@ -1,8 +1,15 @@
 import requests
 import json
+from time import sleep
 
 from bs4 import BeautifulSoup
 import html5lib
+from selenium import webdriver
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+from conf import settings
 
 class ITuens:
     
@@ -24,9 +31,9 @@ class ITuens:
         if (len(elements) >= 1):
             for element in elements:
                 # 半角スペースがプラン名と価格の間にたくさんある為、一旦除去してから再度追加する。
-                billing = element.get_text().replace('\n', '').replace(' ', '').replace('¥', ' ¥')
+                billing = element.get_text().replace('\n', '').replace(' ', '').replace('¥', '\n¥')
                 billing_list.append(billing)
-            billings = '\n'.join(billing_list)
+            billings = '\n\n'.join(billing_list)
     
         return billings
     
@@ -44,13 +51,13 @@ class ITuens:
     該当アプリのiTuensストアでの推定ダウンロード数を取得する
     iTuensはダウンロード数を取得できないため、Google Playでの同アプリのダウンロード数から推定を行う
     '''
-    def getEstimatedDownloadCount(self, download_count: str) -> int:
-        return round(int(download_count) * 0.33)
+    def getEstimatedDownloadCount(self, download_count_google_play: str) -> int:
+        return round(int(download_count_google_play) * 0.33)
 
     '''
     アプリ容量を取得する
     '''
-    def getFileSize(self, country: str, id: int):
+    def getFileSize(self, country: str, id: int) -> str:
         url = self.ituens_api_url_template.format(id, country)
         response = requests.get(url)
         json_obj = json.loads(response.text)
@@ -61,7 +68,7 @@ class ITuens:
     アプリのアイコンを取得する
     取得できなければNoneを返す
     '''
-    def getIconImageOrNone(self, app_name: str, country: str, id: int):
+    def getIconImageOrNone(self, app_name: str, country: str, id: int) -> str:
         url = self.ituens_api_url_template.format(id, country)
         response = requests.get(url)
         result = json.loads(response.text)['results'][0]
@@ -79,5 +86,37 @@ class ITuens:
             return None
 
         icon = requests.get(icon_url)
-        with open('../images/{}'.format(app_name + '_ituens'), 'wb') as file:
+        image_file_name = app_name + '_ituens.png'
+        with open('../images/{}'.format(image_file_name), 'wb') as file:
             file.write(icon.content)
+
+        return image_file_name
+
+    '''
+    アプリのユーザーの国籍ランキングを返す
+    '''
+    def getNationalityRankingOrEmpty(self, url: str) -> str:
+        chrome_service = fs.Service(executable_path=settings.CHROMEDRIVER)
+        driver = webdriver.Chrome(service=chrome_service)
+
+        try:
+            driver.get(url)
+            
+            sleep(1)
+            result_list = []
+            for i in range(1,5):
+                element = driver.find_element(By.CSS_SELECTOR, '#users-country > div > div.right > ul > li:nth-child({})'.format(i))
+                text = element.text.replace('\nアクティブユーザー\n', ':')
+                colon_index = text.find(':')
+                result_list.append(text[:colon_index])
+                newline_index = text.find('\n')
+                result_list.append(text[:newline_index][colon_index+1:] + '\n')
+        except NoSuchElementException:
+            print('要素が取得できませんでした')
+            result = ''
+        else:
+            result = '\n'.join(result_list)
+        finally:
+            driver.quit()
+        
+        return result
